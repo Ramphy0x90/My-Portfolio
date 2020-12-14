415d8ab36fbd0db90d76bde2bb72d459453446a0
ramphy0x90 <ramphy11@hotmail.com>
2020-12-14 23:48:23 +0100

    type writer

--- src/app/container/home/home.component.ts
+++ src/app/container/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as Rellax from '../../../assets/libraries/rellax/rellax.min.js';
+import Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,32 @@ import * as Rellax from '../../../assets/libraries/rellax/rellax.min.js';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public target;
 
-  constructor() { }
+  public options = {loop: true};
+
+  public writer;
+
+  constructor() {}
 
   ngOnInit(): void {
     let rellax = new Rellax('.rellax');
+
+    console.log(this.target);
+
+    this.target = document.querySelector('.tw');
+
+    this.writer = new Typewriter(this.target, {
+      loop: true,
+      typeSpeed: 80,
+      deleteSpeed: 80,
+      typeColor: 'red'
+    });
+
+    this.writer
+      .type("Hi, Im Ramphy a junior developer")
+      .rest(2500)
+      .start()
   }
 
 }
