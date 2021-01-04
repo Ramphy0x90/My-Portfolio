bf937442375bbe7c6cbe82fc8ad64988612509be
ramphy0x90 <ramphy11@hotmail.com>
2021-01-04 15:17:31 +0100

    Sections positions

--- src/app/component/nav-menu/nav-menu.component.ts
+++ src/app/component/nav-menu/nav-menu.component.ts
@@ -54,7 +54,9 @@ export class NavMenuComponent implements OnInit {
       tempNavOptions.forEach(option => {
         let tempOption = document.getElementById(String(option));
         let position = tempOption instanceof HTMLElement ? tempOption.offsetTop : 0;
-        tempSectionPositions[String(option)] = position - window.pageYOffset;
+        let newPosition = position - window.pageYOffset;
+
+        tempSectionPositions[String(option)] = (newPosition > 0) ? newPosition : newPosition + navHeight;
       });
     }
 
