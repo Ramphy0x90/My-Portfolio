2035fc3e800b4ceb069affae9736e327990420ef
ramphy0x90 <ramphy11@hotmail.com>
2021-01-23 13:21:07 +0100

    Scroll logic

--- src/app/component/nav-menu/nav-menu.component.ts
+++ src/app/component/nav-menu/nav-menu.component.ts
@@ -15,21 +15,25 @@ export class NavMenuComponent implements OnInit {
   }
 
   ngOnInit() {
-    window.onload = function() {
-      setSectionsHeight();
-    };
-
-    window.onscroll = function() {
-      stickyMenu();
-      updatePositions();
-    };
-
     let navbar = document.getElementsByClassName('navbar')[0];
     let navHeight = navbar instanceof HTMLElement ? navbar.offsetHeight : 0;
     let sticky = navbar instanceof HTMLElement ? navbar.offsetTop : 0;
 
     let tempNavOptions = this.navOptions;
     let tempSectionPositions = this.sectionPositions;
+
+    let activeNavItem;
+
+    window.onload = function() {
+      setSectionsHeight();
+      colorLinks();
+    };
+
+    window.onscroll = function() {
+      //stickyMenu();
+      updatePositions();
+      colorLinks();
+    };
 
     function stickyMenu() {
       if (window.pageYOffset >= sticky) {
@@ -39,14 +43,34 @@ export class NavMenuComponent implements OnInit {
       }
     }
 
+    function colorLinks() {
+      tempNavOptions.forEach((option, index) => {
+        let navItem = document.getElementById('nav-item-' + index).getElementsByTagName('a')[0];
+        let offsetFromNav = tempSectionPositions[String(option)];
+
+        if (offsetFromNav <= 100 || window.pageYOffset == (document.body.scrollHeight - document.documentElement.offsetHeight)) {
+          activeNavItem = navItem;
+        }
+
+        if (activeNavItem === undefined) {
+          activeNavItem = document.getElementById('nav-item-0').getElementsByTagName('a')[0];
+        }
+
+        navItem.classList.remove('custom-active');
+      });
+
+      activeNavItem.classList.add('custom-active');
+    }
+
     function setSectionsHeight() {
-      const docHeight = window.innerHeight
+      const docHeight = window.innerHeight;
 
       tempNavOptions.forEach(option => {
         let tempOption = document.getElementById(String(option));
 
-        if(option !== 'contact-me')
-          tempOption.style.height = (docHeight + navHeight) + 'px';
+        if (option !== 'contact-me') {
+          tempOption.style.height = (docHeight - navHeight) + 'px';
+        }
       });
     }
 
@@ -56,7 +80,8 @@ export class NavMenuComponent implements OnInit {
         let position = tempOption instanceof HTMLElement ? tempOption.offsetTop : 0;
         let newPosition = position - window.pageYOffset;
 
-        tempSectionPositions[String(option)] = (newPosition > 0 || position == 0) ? newPosition : newPosition + navHeight;
+        //tempSectionPositions[String(option)] = (newPosition > 0 || position == 0) ? newPosition : newPosition + navHeight;
+        tempSectionPositions[String(option)] = newPosition - navHeight;
       });
     }
 
