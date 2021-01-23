ea263c9e5f3ffb6066fc789e290f41a076aa759c
ramphy0x90 <ramphy11@hotmail.com>
2021-01-23 17:59:03 +0100

    Reformat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './container/home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavMenuComponent } from './component/nav-menu/nav-menu.component';
-import { SplitPipe } from './pipes/split.pipe';
-import { StripPipe } from './pipes/strip.pipe';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './container/home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NavMenuComponent} from './component/nav-menu/nav-menu.component';
+import {SplitPipe} from './pipes/split.pipe';
+import {StripPipe} from './pipes/strip.pipe';
 import {ScrollToModule} from '@nicky-lenaers/ngx-scroll-to';
-import { SkillsComponent } from './component/skills/skills.component';
-import { WorkExperienceComponent } from './component/work-experience/work-experience.component';
-import { AboutMeComponent } from './component/about-me/about-me.component';
-import { ProjectsComponent } from './component/projects/projects.component';
-import { ContactMeComponent } from './component/contact-me/contact-me.component'
-import { NgParticlesModule } from 'ng-particles';
-import { ParticlesComponent } from './component/particles/particles.component';
+import {SkillsComponent} from './component/skills/skills.component';
+import {WorkExperienceComponent} from './component/work-experience/work-experience.component';
+import {AboutMeComponent} from './component/about-me/about-me.component';
+import {ProjectsComponent} from './component/projects/projects.component';
+import {ContactMeComponent} from './component/contact-me/contact-me.component';
+import {NgParticlesModule} from 'ng-particles';
+import {ParticlesComponent} from './component/particles/particles.component';
 
 @NgModule({
   declarations: [
@@ -41,4 +41,5 @@ import { ParticlesComponent } from './component/particles/particles.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
