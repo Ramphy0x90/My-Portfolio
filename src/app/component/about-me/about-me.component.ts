0f1f296bcb6bc66688c6b372726c26ee1d7a2527
ramphy0x90 <ramphy11@hotmail.com>
2021-01-27 22:55:03 +0100

    About me responsive styles

--- src/app/component/about-me/about-me.component.ts
+++ src/app/component/about-me/about-me.component.ts
@@ -9,6 +9,9 @@ import 'aos/dist/aos.css';
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
+  public navbar: any;
+  public docHeight: number;
+  public navHeight: number;
 
   text_1: string = 'I am a highly motivated and self-taught person. I have good knowledge in Java, Php and MySql, acquired during my apprenticeship as an application developer. In addition, I have knowledge in Python, Ionic, Angular and Assembly x64 learned independently.';
 
@@ -17,6 +20,21 @@ export class AboutMeComponent implements OnInit {
 
   ngOnInit(): void {
     AOS.init();
+
+    window.addEventListener('load', this.onLoad, true);
+  }
+
+  onLoad(){
+    this.navbar = document.querySelector('#main-nav');
+    this.docHeight = window.innerHeight;
+    this.navHeight = this.navbar.offsetHeight;
+
+    let mainContainer = document.getElementById("main-container");
+    let padding = ((this.docHeight - this.navHeight) - mainContainer.offsetHeight) / 2;
+
+    mainContainer.style.padding = padding + "px 0";
+
+    console.log(padding)
   }
 
 }
