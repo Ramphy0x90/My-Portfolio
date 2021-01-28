0f8692da4f7a83747d658facbba3d4fd4cb9f322
ramphy0x90 <ramphy11@hotmail.com>
2021-01-28 14:06:56 +0100

    General styles

--- src/app/component/projects/projects.component.ts
+++ src/app/component/projects/projects.component.ts
@@ -9,11 +9,28 @@ import {DataService} from '../../service/data.service';
 export class ProjectsComponent implements OnInit {
   public projects: Object[];
 
+  public navbar: any;
+  public docHeight: number;
+  public navHeight: number;
+
   constructor(private dataService: DataService) {
     this.projects = this.dataService.getProjects();
   }
 
   ngOnInit(): void {
+    window.addEventListener('load', this.onLoad, true);
   }
 
+  onLoad(){
+    this.navbar = document.querySelector('#main-nav');
+    this.docHeight = window.innerHeight;
+    this.navHeight = this.navbar.offsetHeight;
+
+    let mainContainer = document.getElementById("main-container-p");
+    let padding = ((this.docHeight - this.navHeight) - mainContainer.offsetHeight) / 2;
+
+    mainContainer.style.padding = padding + "px 0";
+
+    console.log(padding)
+  }
 }
